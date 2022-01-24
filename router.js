1e339d6f882990dd865cc361542181eba8c9a9ce
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-24 12:42:43 +0200

    Requesting API data (Stories)

--- router.js
+++ router.js
@@ -13,7 +13,7 @@ export default class RouterHandler {
             {path: '/new', page: Stories },
             {path: '/ask', page: Stories },
             {path: '/show', page: Stories },
-            {path: '/favorites', page: Stories },
+            
         ];
 
         routes.forEach(({path, page}) => {
